a3d3a6a19e72e8c4a8e9ab85965642d2e1e82582
WuXiaoLian <wuxiaolian@luoyideiMac.local>
2018-06-07 15:34:11 +0800

    Initial Commit

---
+++ TagViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TagViewDemo
+//
+//  Created by Wuxiaolian on 2018/6/7.
+//  Copyright © 2018年 Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TagViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TagViewDemo
+//
+//  Created by Wuxiaolian on 2018/6/7.
+//  Copyright © 2018年 Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
